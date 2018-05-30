2abf19867577552ce2afaa605f991e6406fa8ff6
水野 久代 <hisayo@mizuno-no-macbook-pro.local>
2018-05-30 09:59:51 +0900

    修正

--- SecondKadaiApp/ViewController.swift
+++ SecondKadaiApp/ViewController.swift
@@ -21,6 +21,7 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
         
     }
+    //fistTextデータ
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let resultViewController:ResultViewController = segue.destination as! ResultViewController
         resultViewController.nextLabel = fstTextbox.text!
